9f0840c64aba0305fa37f5002b0971a315b6d9aa
Soykot Alam <a.k.mshahalam68@gmail.com>
2022-12-17 02:29:51 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+const fish =  { id: 58,name: 'King Hilsha', price:9000, phone: '0171555555555',address: 'Chadpur', dress: 'silver'};
+// const phone = fish.phone;
+// const price = fish.price;
+// const dress = fish.dress;
+// const id = fish.id;
+
+const {phone, price,dress,id} = { id: 58,name: 'King Hilsha', price:9000, phone: '0171555555555',address: 'Chadpur', dress: 'silver'};
+
+console.log(phone,price);
+console.log(phone);
+console.log(phone,dress);
+console.log(phone,id);
+console.log(phone);
