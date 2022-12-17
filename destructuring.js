d8ffc4c9d199e368e1de05a437b9182a23b23b6f
Soykot Alam <a.k.mshahalam68@gmail.com>
2022-12-17 23:20:33 +0600

    l

--- destructuring.js
+++ destructuring.js
@@ -11,3 +11,27 @@ console.log(phone);
 console.log(phone,dress);
 console.log(phone,id);
 console.log(phone);
+
+const company = { 
+    name: 'GP', 
+    ceo:{id: 1, name: 'ajmol', food: 'fuchka'},
+    web:{
+        work: 'website development', 
+        employee:22, framework:'react',
+    tech:{
+        first:'html', 
+         second:'css',
+         third:'js'
+        }
+},
+};
+// const work = company.web.work;
+// const framework = company.web.framework;
+
+const {work, framework} = company.web;
+
+const {food} = company.ceo;
+
+const {second,third} = company.web.tech;
+console.log(work,framework,food,second,third);
+
